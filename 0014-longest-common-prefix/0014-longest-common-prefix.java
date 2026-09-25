ca5150fc35f74b4f31729f1612db792e71136e59
mayankmeena6996 <mayankmeena6996@gmail.com>
2026-09-25 23:25:33 +0530

    Time: 1 ms (59.85%), Space: 43.5 MB (16.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i = 0; i < strs[0].length(); i++) {
             char ch = strs[0].charAt(i);
             for(int j = 1; j < strs.length; j++) {
-
                 if(i >= strs[j].length() || strs[j].charAt(i) != ch) {
                     return sb.toString();
                 }
